9cc04036db25f44e761555e4fd3ff0b4c0b3fb55
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-03 21:29:29 +0530

    added a class to handle transaction APi's

---
+++ lib/ost-sdk-ruby/saas/transaction.rb
@@ -0,0 +1,35 @@
+module OSTSdk
+
+  module Saas
+
+    class Transaction < OSTSdk::Saas::Base
+
+      # Initialize
+      #
+      # Arguments:
+      #   environment: (String)
+      #   credentials: (OSTSdk::Util::APICredentials)
+      #   api_spec: (Boolean)
+      #
+      def initialize(environment, credentials, api_spec = false)
+        super
+        @url_prefix = '/transaction/'
+      end
+
+      # Execute a transaction
+      #
+      # Arguments:
+      #   params: (Hash)
+      #
+      # Returns:
+      #   response: (OSTSdk::Util::Result)
+      #
+      def execute(params)
+        http_helper.send_post_request("#{@url_prefix}/execute", params)
+      end
+
+    end
+
+  end
+
+end
