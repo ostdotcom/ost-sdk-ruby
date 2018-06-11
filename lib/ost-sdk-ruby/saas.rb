7fdf7d6ef8721cedcc217ee377a22a4465e1dc20
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-06-11 12:56:00 +0530

    added balances module to V1 API's

--- lib/ost-sdk-ruby/saas.rb
+++ lib/ost-sdk-ruby/saas.rb
@@ -13,6 +13,7 @@ require_relative 'saas/v1/actions'
 require_relative 'saas/v1/airdrops'
 require_relative 'saas/v1/transfers'
 require_relative 'saas/v1/transactions'
+require_relative 'saas/v1/balances'
 
 module OSTSdk
 
