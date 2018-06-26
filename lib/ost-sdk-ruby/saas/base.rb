4362da797d7cc83e964fdc274ed746f064e146b6
Puneet Khushwani <puneet.khushwani@ost.com>
2018-06-26 17:33:02 +0530

    fix to have patch versions hit minor version API endpoints

--- lib/ost-sdk-ruby/saas/base.rb
+++ lib/ost-sdk-ruby/saas/base.rb
@@ -22,7 +22,7 @@ module OSTSdk
         fail 'missing API Key' if params[:api_key].nil?
         fail 'missing API Secret' if params[:api_secret].nil?
 
-        params[:api_base_url].gsub!(/\/$/, '') # remove trailing slash
+        params[:api_base_url] = sanitize_api_base_url(params[:api_base_url])
 
         @http_helper = OSTSdk::Util::HTTPHelper.new(params)
 
@@ -49,6 +49,32 @@ module OSTSdk
           fail "id missing in request params"
         end
         id
+      end
+
+      # Sanitize API Base URL
+      #
+      # Arguments:
+      #   params: (String)
+      #
+      # Returns:
+      #   api_base_url: (String)
+      #
+      def sanitize_api_base_url(api_base_url)
+
+        api_base_url = api_base_url.gsub(/\/$/, '') # remove trailing slash
+
+        version_str = api_base_url.split('/')[-1]
+
+        return api_base_url unless version_str.include?('v')
+
+        buffer = version_str.split('.')
+        if buffer.length == 3 # dot count in version == 2 ie. version had a patch version ex. v1.0.0
+          regex_match_rsp = /^(.+)(\.\d{1,3})(\/{0,1})$/.match(api_base_url)
+          api_base_url = regex_match_rsp[1]
+        end
+
+        return api_base_url
+
       end
 
     end
