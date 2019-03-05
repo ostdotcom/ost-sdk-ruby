3704edb0571b4a922ed24d9f87448b8930d4dcd7
aman-barbaria <amanbarbaria@gmail.com>
2019-03-05 13:25:00 +0530

    updated test case for signature validation

--- lib/ost-sdk-ruby/test/signature_generation_test.rb
+++ lib/ost-sdk-ruby/test/signature_generation_test.rb
@@ -6,30 +6,28 @@ require_relative "../../../lib/config"
 class SignatureGenerationTest < Test::Unit::TestCase
 
   def signature_test_obj
-    params = {api_secret: "35f346e5ef825ed4499da98a6ac6b401"}
+    params = {
+        api_secret: "a0431203671f42c079b2154066fd04ba"
+    }
     @signature_test_obj ||= OSTSdk::Util::HTTPHelper.new(params)
   end
 
   def test_signature
     test_obj_for_signature = {
-        k1: 'Rachin',
-        k2: 'tejas',
-        list2: [
-            {a: 'L21A', b: 'L21B'},
-            {a: 'L22A', b: 'L22B'},
-            {a: 'L23A', b: 'L23B'}
-        ],
-        make_mistakes: "",
-        nice_param: [],
-        empty_obj: {},
-        empty_str: '',
-        garbage_str: "~^[]%$#@!&*~,./?~()-_'this is garbage",
-        id: 11003,
-        email: 'mayur+67@ost.com'
+        a: nil,
+        b: [],
+        c: '',
+        d: ["", nil],
+        e: {},
+        k1: 125.45,
+        k2: 'Tejas',
+        aaaaa: {a: {a: "L21A", b: "L21B"}, b: {a: "L22A", b: "L22B"}, c: {a: "L23A", b: "L23B"}},
+        arrayValues: ["Hello", "There", "12345"],
+        garbage_str: "~!@#$%^&*()_+-= {}[]:\";'?/<>,. this is garbage"
     }
-    test_endpoint = "/users"
-    signature = signature_test_obj.get_signature_for_test(test_endpoint,test_obj_for_signature)
-    assert_equal(signature, "22ad0a29668ad4b85604f828370d8cd12b6c925c35b27fb7b7f401379ee32a27")
+    test_endpoint = "/api/v2/users"
+    signature = signature_test_obj.get_signature_for_test(test_endpoint, test_obj_for_signature)
+    assert_equal(signature, "28664cdbc613b66835d7bcf825dce719fb8e0621992c291ba9bd1767c1c5560d")
   end
 
 end
